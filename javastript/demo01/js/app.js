4a6cea32de4a1d7fc66f5a910a0d78c1993cced7
YANJUL0301 <ventas@focusfotoyvideo.com>
2024-10-12 23:38:13 +0000

    validar que las cajas no esten vacias

---
+++ javastript/demo01/js/app.js
@@ -0,0 +1,26 @@
+
+const formularioregistro = document.getElementById("formulario-registro")
+const mensajeserror = document.getElementById("mensajeserror")
+formularioregistro.addEventListener("submit", function (event) {
+    event.preventDefault
+    const usuario = document.getElementById("usuario").value;
+    const correo = document.getElementById("correo").value;
+    const contrasena = document.getElementById("contrasena")
+    mensajeserror = this.innerHTML = "";
+    if (!usuario.trim()) {
+        mostrarerror("el nombre de usuario es obligatorio");
+        return
+    }
+    if (!correo.trim()) {
+        mostrarerror("el correo es obligatorio");
+        return
+    }
+    if (!contrasena.trim()) {
+        mostrarerror("la contraseña es obligatoria");
+        return
+}
+function mostrarerror(mensaje) 
+const mensajeerror = document.createElement("div")
+mensajeerror.className = "error";
+mensajeerror.textContent = mensaje;
+mensajeserror.appendChild(mensajeerror);
