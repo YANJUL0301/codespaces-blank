ba0a1a34c1b4053c6328960db2e8460e81f5d5a8
YANJUL0301 <ventas@focusfotoyvideo.com>
2024-10-12 23:57:15 +0000

    fin

--- javastript/demo01/js/app.js
+++ javastript/demo01/js/app.js
@@ -1,26 +1,30 @@
 
 const formularioregistro = document.getElementById("formulario-registro")
 const mensajeserror = document.getElementById("mensajeserror")
+
 formularioregistro.addEventListener("submit", function (event) {
-    event.preventDefault
+    event.preventDefault;
     const usuario = document.getElementById("usuario").value;
     const correo = document.getElementById("correo").value;
-    const contrasena = document.getElementById("contrasena")
-    mensajeserror = this.innerHTML = "";
+    const contrasena = document.getElementById("contrasena").value;
+    mensajeserror.innerHTML = "";
     if (!usuario.trim()) {
         mostrarerror("el nombre de usuario es obligatorio");
-        return
+        return;
     }
     if (!correo.trim()) {
         mostrarerror("el correo es obligatorio");
-        return
+        return;
     }
     if (!contrasena.trim()) {
         mostrarerror("la contraseña es obligatoria");
-        return
+        return;
+    }
+
+})
+function mostrarerror(mensaje) {
+    const mensajeerror = document.createElement("div")
+    mensajeerror.className = "error";
+    mensajeerror.textContent = mensaje;
+    mensajeserror.appendChild(mensajeerror);
 }
-function mostrarerror(mensaje) 
-const mensajeerror = document.createElement("div")
-mensajeerror.className = "error";
-mensajeerror.textContent = mensaje;
-mensajeserror.appendChild(mensajeerror);
